a82feccc4eeff8fe8fe2abd65042eced9edab50e
MihailBurdin <144234023+MihailBurdin@users.noreply.github.com>
2023-10-02 14:03:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,98 @@
+#2
+#x = int(input())
+#y = int(input())
+#z = int(input())
+#print(x + z + y)
+
+#3
+#x=int(input())
+#print('Плошадь куба ',x **3)
+#print('Объем куба',6*x **2)
+
+#4
+#a = int(input())
+#b = int(input())
+#print(3*(a + b)**3 + 275*b**2 -127*a - 41)
+
+#5
+#x = int(input())
+#print('Следующее за числом', x ,'число', x+1)
+#print('Для числа',x,'предыдущее число',x-1)
+
+#6
+#x=int(input())
+#y=int(input())
+#z=int(input())
+#q=int(input())
+#print(3*(z+y+q+x))
+
+#7
+#a_1=int(input())
+#d=int(input())
+#n=int(input())
+#print(a_1+d*(n-1) )
+
+#8
+#x=int(input())
+#print(x,2*x,3*x,4*x,5*x,sep='---')
+
+#9
+#a=int(input())
+#b=int(input())
+#print(b // a)
+#print(b % a)
+
+#10
+#a=int(input())
+#print(a//2 + a%2)
+
+#11
+#x=int(input())
+#z=x//60
+#q=x-(z*60)
+#print(x,'мин это',z,'час',q,'минут')
+
+#12
+#x=int(input())
+#a=x%10
+#b=(x%100)//10
+#c=x//100
+#print('Сумма цифр',a + b + c)
+#print('Произведение цифр',a * b * c)
+
+#13
+#x= int(input())
+#print('     ~     '*x)
+#print('    - -    '*x)
+#print('   (о о)   '*x)
+#print(' /   ^   / '*x)
+#print('/(   _   )/'*x)
+#print('  ^^   ^^  '*x)
+
+#14
+#abc = int(input())
+#k = 1
+#n = (abc // 10 ** k) % 10
+#print(n)
+#15
+#x = int(input())
+#hours = x % (60 * 24) // 60
+#minutes = x % 60
+#print(hours, minutes)
+#16
+#x = int(input())
+#print(1 - x)
+#17
+#x = int(input())
+#print((x//2+1)*2)
+#18
+#y = int(input())
+#t = int(input())
+#x = y * t
+#n = x // 109
+#print(-(109 * n - x))
+#19
+#q = float(input())
+#a = float(input())
+#b = float(input())
+#print(int(1 + (q - b - 1) / (a - b)))
